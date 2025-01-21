e873516f7f8643575fe5b01ca120c1b5d4f5630a
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-21 09:45:17 +0000

    No commits

--- Portfolio.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#fullpage', {
-    autoScrolling: true,
-    fitToSection: true,
-    scrollingSpeed: 700,
-    scrollHorizontally: false,
-    scrollOverflow: true,
-    licenseKey: 'YOUR_KEY_HERE'
-});
