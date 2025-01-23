e22016ce6d2d8ec05ccde2099f529779b8a7e51b
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-23 14:32:40 +0000

    Added animationts to the text with JavaScript

---
+++ app.js
@@ -0,0 +1,14 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    } else {
+      entry.target.classList.remove('show');
+    }
+  });
+});
+
+const sections = document.querySelectorAll('.TextOnSlides');
+sections.forEach((el) => {
+  observer.observe(el);
+});
