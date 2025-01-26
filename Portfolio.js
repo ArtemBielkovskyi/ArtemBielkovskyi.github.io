af9f34de25cb7035cb2e8f8afb3618c901740953
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-26 10:09:56 +0000

    Fixed issue with an anchor link when it was scrolling it would scroll back up, fixed using fullpage methods

--- Portfolio.js
+++ Portfolio.js
@@ -5,10 +5,29 @@ new fullpage('#fullpage', {
     scrollHorizontally: false,
     scrollOverflow: true,
     licenseKey: 'YOUR_KEY_HERE',
-    afterRender: function(){
-        // Disable default scroll
-        document.addEventListener('wheel', function(e) {
-            e.preventDefault();
-        }, { passive: false });
+    //Header section animation
+    anchors: ['about', 'experience', 'contact'],
+    menu: '.header',
+    onLeave: function(origin, destination, direction){
+        // Removes the animation class when leaving the section
+        if (origin.index === 0) {
+            document.querySelectorAll('.link').forEach(link => {
+                link.classList.remove('animate');
+            });
+        }
+    },
+    afterLoad: function(origin, destination, direction){
+        // Adds the animation class when the section is loaded
+        if (destination.index === 0) {
+            document.querySelectorAll('.link').forEach(link => {
+                link.classList.add('animate');
+            });
+            document.querySelector('.header').style.opacity = '1';
+            document.querySelector('.header').style.filter = 'blur(0)';
+            document.querySelector('.header').style.transform = 'translateY(0)';
+        }
     }
 });
+
+
+
