ade8a4f5eadc9675f5333920abb8aebd3e66a758
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-28 16:16:44 +0000

    added picture on the 'About me' page need to animate it still

--- app.js
+++ app.js
@@ -27,3 +27,19 @@ const sections2 = document.querySelectorAll('.AboutMeInfo');
 sections2.forEach((el) => {
 observer2.observe(el);
 });
+
+
+const observer3 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+  if (entry.isIntersecting) {
+    entry.target.classList.add('showImg');
+  } else {
+    entry.target.classList.remove('showImg');
+  }
+});
+});
+
+const sections3 = document.querySelectorAll('.AboutMeImage');
+sections3.forEach((el) => {
+observer3.observe(el);
+});
