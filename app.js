6984b545c5cc48cba406377d63e6d02a81981a98
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-01-28 09:52:20 +0000

    Started 'About me' section

--- app.js
+++ app.js
@@ -12,3 +12,18 @@ const sections = document.querySelectorAll('.TextOnSlides');
 sections.forEach((el) => {
   observer.observe(el);
 });
+
+const observer2 = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+  if (entry.isIntersecting) {
+    entry.target.classList.add('show2');
+  } else {
+    entry.target.classList.remove('show2');
+  }
+});
+});
+
+const sections2 = document.querySelectorAll('.AboutMeInfo');
+sections2.forEach((el) => {
+observer2.observe(el);
+});
