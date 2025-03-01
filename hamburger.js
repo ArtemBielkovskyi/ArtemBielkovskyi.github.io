f07e34f119d5fbbbdab7bd297cbcf02199d7f156
ArtemBielkovskyi <157801683+ArtemBielkovskyi@users.noreply.github.com>
2025-03-01 16:31:51 +0000

    Working on slider hamburger menu

---
+++ hamburger.js
@@ -0,0 +1,11 @@
+// function hamburgerOnCLick(active) {
+//   const hamburger = document.getElementById('hamburger');
+//   const links = document.getElementsByClassName('link');
+//   if (active) {
+//     hamburger.classList.add('active');
+//     links.classList.add('active');
+//   } else {
+//     hamburger.classList.remove('active');
+//     links.classList.remove('active');
+//   }
+// }
